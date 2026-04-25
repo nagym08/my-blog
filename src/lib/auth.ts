2888b75b309a1757f33da6ce1b0e6204aef4d4f6
NamelessCoder <nagymiki08@gmail.com>
2026-04-25 20:49:20 +0200

    setup neon support

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
-import Facebook from "next-auth/providers/facebook";
-import Twitter from "next-auth/providers/twitter";
+// import Google from "next-auth/providers/google";
+// import Facebook from "next-auth/providers/facebook";
+// import Twitter from "next-auth/providers/twitter";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "./db";
 import {
@@ -19,7 +19,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     sessionsTable: sessions,
     verificationTokensTable: verificationTokens,
   }),
-  providers: [GitHub, Google, Facebook, Twitter],
+  providers: [GitHub],
   session: {
     strategy: "database",
   },
